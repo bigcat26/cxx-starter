a069e27f76466ae8495152de1393afaa1aa9cb9c
chris <rc.local@qq.com>
2022-06-16 16:31:48 +0800

    update project settings

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # CXX Quick Start Project
 
+A skeleton project aim to start my C++ project as quick as possible
+
+## Requirements
+
 - CMake
+- Ninja
 - VSCode
+
+## Usage
+
+```shell
+git clone https://github.com/bigcat26/cxx-starter.git
+code -a cxx-starter
+```
+
+- `cmd + shift + p:  CMake Configure`
+- `cmd + shift + p:  CMake Build`
+- `F5`: start debug run
--- app/main.cc
+++ app/main.cc
@@ -1,5 +1,3 @@
-#include "lib.h"
-
 #include <algorithm>
 #include <cmath>
 #include <functional>
@@ -12,9 +10,25 @@
 #include <string>
 #include <vector>
 
+#include "lib.h"
+#include "spdlog/logger.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/stdout_sinks.h"
+#include "spdlog/spdlog.h"
+
 using namespace std;
 
+static void initLogger(void) {
+    spdlog::set_level(spdlog::level::trace);
+    // auto sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(path, size, rotate, true);
+    // sink->should_log(spdlog::level::trace);
+    // spdlog::default_logger()->sinks().push_back(sink);
+    // spdlog::default_logger()->flush_on(spdlog::level::trace);
+}
+
 int main(int argc, char *argv[]) {
-  cout << "Hello, world" << endl;
-  return 0;
+    initLogger();
+    SPDLOG_INFO("Hello, world");
+
+    return 0;
 }
